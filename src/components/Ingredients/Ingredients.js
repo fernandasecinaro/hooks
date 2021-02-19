43a13d88e559415c3fede56a4ff63a15507ff5f8
Fernanda Secinaro <secinarof@gmail.com>
2021-02-18 22:50:58 -0300

    hasta 434 inc

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,19 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList';
 import Search from './Search';
 
-function Ingredients() {
+const Ingredients = () => {
+  const [userIngredients, setUserIngredients] = useState([]);
+
+  const addIngredientHandler = ingredient => {
+    fetch('https://react-hooks-update-be647-default-rtdb.firebaseio.com/ingredients.json', {
+      method: 'POST',
+      body: JSON.stringify({ ingredient }),
+      headers: { 'Content-Type': 'application/json' },
+    })
+      .then(response => {
+        return response.json();
+      })
+      .then(responseData => {
+        setUserIngredients(prevIngredients => [...prevIngredients, { id: responseData.name, ...ingredient }]);
+      });
+  };
+
+  const removeIngredientHandler = ingredientId => {
+    setUserIngredients(prevIngredients =>
+      prevIngredients.filter(ingredient => {
+        return ingredient !== ingredientId;
+      })
+    );
+  };
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
         <Search />
-        {/* Need to add list here! */}
+        <IngredientList ingredients={userIngredients} onRemoveItem={removeIngredientHandler} />
       </section>
     </div>
   );
-}
+};
 
 export default Ingredients;
